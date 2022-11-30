b7dbf78e11ed8a2e74953155296a778aaacc903c
ozonetel <ozonetel@ozonetel-Latitude-3490>
2022-11-30 19:35:20 +0530

    This is my first git sharing

---
+++ orange/src/test/java/CheckGit/GitProject.java
@@ -0,0 +1,12 @@
+package CheckGit;
+
+public class GitProject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is my firtst test case");
+
+	}
+
+}
